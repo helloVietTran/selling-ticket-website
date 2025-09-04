7db38523766a6c3870e6a98ed94abe7004ac5948
helloVietTran <trandanhviet192003@gmail.com>
2025-09-04 11:00:50 +0700

    Feat: init header

--- frontend/src/features/organizer/pages/events-page.tsx
+++ frontend/src/features/organizer/pages/events-page.tsx
@@ -1,12 +1,24 @@
+import React from 'react';
+
 import EventItem, { mockEvents } from '@/features/organizer/components/event-item';
-import SearchBox from '@/features/organizer/components/search-box';
 import StatusTabs from '@/components/status-tabs';
+import SearchBox from '@/components/search-box';
 
 const EventsPage = () => {
+  const [query, setQuery] = React.useState('');
+
+  const handleSearch = () => {
+    console.log('Searching for:', query);
+  };
   return (
     <>
       <div className="flex items-center justify-between pb-6">
-        <SearchBox />
+        <SearchBox
+          placeholder="Tìm sự kiện"
+          value={query}
+          onChange={setQuery}
+          onSearch={handleSearch}
+        />
         <StatusTabs />
       </div>
       <div className="flex flex-col gap-4">
