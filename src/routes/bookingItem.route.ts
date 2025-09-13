c41f0f7065bdbe9f4248de0114b3c8e596f65ce8
helloVietTran <trandanhviet192003@gmail.com>
2025-09-13 07:00:48 +0700

    Revert "Merge pull request #40 from helloVietTran/ha/develop/be"
    This reverts commit 25eef9b2b20c5a48feaa2c251205b1c3546bb186, reversing
    changes made to a2cdc3fbd92ab1b531d53ff44817ccfe86710a9b.

--- src/routes/bookingItem.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import bookingItemController from '../controllers/booking-item.controller';
-
-const router=Router();
-
-router.post('/',bookingItemController.creatBookingItem)
-
-export default router
