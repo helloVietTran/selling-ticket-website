ab4951a002873a336f7e067e7faeea8351392d65
helloVietTran <trandanhviet192003@gmail.com>
2025-09-18 11:29:56 +0700

    Feat(api): complete event api

--- frontend/src/api/eventApi.ts
+++ frontend/src/api/eventApi.ts
@@ -1,12 +1,32 @@
 import api from "./api";
-import type { Event } from "./types/types";
+import type { GetEventsParams } from "./types/params.type";
+import type { BaseResponse, Event } from "./types/response.type";
 
-
-async function createFullEvent(payload: any): Promise<Event> {
+async function createFullEvent(payload: any): Promise<BaseResponse<Event>> {
   const res = await api.post("/events", payload);
   return res.data;
 }
 
+async function getEvents(params: GetEventsParams): Promise<BaseResponse<Event[]>> {
+  const res = await api.get("/events", {
+    params: {
+      startTime: params.startTime,
+      endTime: params.endTime,
+      category: params.category,
+      province: params.province,
+      keyword: params.keyword
+    },
+  });
+
+  return res.data;
+}
+
+async function deleteEvent(eventId: string): Promise<void>{
+  await api.delete(`/events/${eventId}`);
+}
+
 export {
-  createFullEvent
+  createFullEvent, 
+  getEvents,
+  deleteEvent
 }
