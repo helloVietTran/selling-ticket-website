7d7a248ebfc18813a2325173fe55f7b80cdb71cc
havanthe <hathe242424@gmail.com>
2025-09-27 07:32:54 +0700

    fix(route): update revenue route logic

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,13 +7,9 @@ import ticketTypeRoutes from './ticket-type.route';
 
 import ticketRoutes from './ticket.route';
 import userRoute from './user.route';
-<<<<<<< HEAD
+
 import bookingRoute from './booking.route'
 import revenueRoute from './revenue.route';
-=======
-import bookingRoute from './booking.route';
->>>>>>> 66c963f403a5499c8bb667ed1949effb8dc4b849
-
 function route(app: Application) {
   const urlPrefix = config.api_prefix + config.api_version;
 
@@ -23,10 +19,8 @@ function route(app: Application) {
   app.use(`${urlPrefix}/ticket`, ticketRoutes);
   app.use(`${urlPrefix}/user`, userRoute);
   app.use(`${urlPrefix}/booking`, bookingRoute);
-<<<<<<< HEAD
-  app.use(`${urlPrefix}`, revenueRoute);
-=======
->>>>>>> 66c963f403a5499c8bb667ed1949effb8dc4b849
+  app.use(`${urlPrefix}/revenue`, revenueRoute);
+
 }
 
 export default route;
